23dd6281f1319a7836e480301b3f2c5e08c392dd
Chosen2730 <70141158+Chosen2730@users.noreply.github.com>
2022-03-10 20:45:38 +0100

    Updates

---
+++ precache-manifest.11fb03c9a576c5fa01f94b4b24fc34ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "658949055e901a9a498023d939a2317d",
+    "url": "/Tours/index.html"
+  },
+  {
+    "revision": "240d1b50425524352833",
+    "url": "/Tours/static/css/main.36cfb1f0.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/Tours/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tours/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "240d1b50425524352833",
+    "url": "/Tours/static/js/main.5130c7d4.chunk.js"
+  },
+  {
+    "revision": "3128dff8851166541ea2",
+    "url": "/Tours/static/js/runtime-main.83f56e71.js"
+  }
+]);
